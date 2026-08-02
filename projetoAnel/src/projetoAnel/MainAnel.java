faec188d321c5326adffc0cca417cdd67aa00252
joaoantonioso <joaoantonios.soaress@gmail.com>
2026-08-02 12:16:50 -0300

    main atualizado

--- projetoAnel/src/projetoAnel/MainAnel.java
+++ projetoAnel/src/projetoAnel/MainAnel.java
@@ -1,7 +1,10 @@
 package projetoAnel;
 
+import java.util.*;
+
 public class MainAnel {
-public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("=".repeat(27));
@@ -13,7 +16,7 @@ public static void main(String[] args) {
 		int quantElem = sc.nextInt();
 		sc.nextLine();
 		
-		Set<Integer> conjunto = definirConjunto(quantElem, sc);
+		LinkedHashSet<Integer> conjunto = definirConjunto(quantElem, sc);
 		System.out.println("Elementos do conjunto: " + conjunto);
 		
 		Map<Integer, int[]> tabelaAdicao = definirTabela("adição", quantElem, sc);
@@ -26,36 +29,46 @@ public static void main(String[] args) {
 		exibirTabela(tabelaMultiplicacao);
 		System.out.println("");
 		
+		int[][] tabelaAdicaoAjustada = ajustarTabela(tabelaAdicao);
+		int[][] tabelaMultiplicacaoAjustada = ajustarTabela(tabelaMultiplicacao);
+		
+		Anel anel = new Anel(tabelaAdicaoAjustada, tabelaMultiplicacaoAjustada, conjunto);
+		
+		System.out.println(anel.exibirInformacoes());
+		
 	}
 	
-	public static Set<Integer> definirConjunto(int quantElem, Scanner sc) {
-		Set<Integer> conjunto = new LinkedHashSet<>();
+	public static LinkedHashSet<Integer> definirConjunto(int quantElem, Scanner sc) {
+		LinkedHashSet<Integer> conjunto = new LinkedHashSet<>();
 		
 		System.out.println("=".repeat(32));
 		System.out.println("DEFINIR OS ELEMENTOS DO CONJUNTO");
 		System.out.println("=".repeat(32));
 		
-		System.out.println("Quais os elementos desse conjunto? (separado por espaço): ");
-		for (int i = 0; i < quantElem; i++) {
-			System.out.println(String.format("%d° elemento: ", i+1));
-			int num = sc.nextInt();
-			sc.nextLine();
-			if (conjunto.contains(num)) {
-				System.out.println("Esse número já está inserido no conjunto!");
-				i--;
-			}
-			
-			conjunto.add(num);
-		}
-		
-		return conjunto;
+		System.out.println("Quais os elementos desse conjunto? (um por vez): ");
+		int i = 0;
+	    while (i < quantElem) {
+	        System.out.printf("%d° elemento: ", i + 1);
+	        int num = sc.nextInt();
+	        sc.nextLine();
+	        
+	        if (conjunto.contains(num)) {
+	            System.out.println("Esse número já está inserido no conjunto! Digite outro.");
+	            continue;  
+	        }
+	        
+	        conjunto.add(num);
+	        i++;  
+	    }
+	    
+	    return conjunto;
 	}
 	
 	public static Map<Integer, int[]> definirTabela(String operacao, int quantElem, Scanner sc) {
 		Map<Integer, int[]> elementosPorLinha = new LinkedHashMap<>();
 		
 		System.out.println("=".repeat(32));
-		System.out.println("DEFINIR A TABELA DE " + operacao);
+		System.out.println("DEFINIR A TABELA DE " + operacao.toUpperCase());
 		System.out.println("=".repeat(32));
 		
 		System.out.println(String.format("Quais os elementos da tabela de %s? (separado por espaço)", operacao));
@@ -82,4 +95,21 @@ public static void main(String[] args) {
 			System.out.println("");
 		}
 	}
+	
+	public static int[][] ajustarTabela(Map<Integer, int[]> tabela) {
+		int tam = tabela.size();
+		int[][] tabelaFinal = new int[tam][tam];
+		
+		for (int i = 0; i < tam; i++) {
+			int[] linha = tabela.get(i);
+			for (int j=0; j < tam; j++) {
+				tabelaFinal[i][j] = linha[j];
+			}
+		}
+		
+		
+		return tabelaFinal;
+	}
+	
 }
+
